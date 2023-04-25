7e3726828761c9e801649739819b62654d8a3f3e
mahteixeira <teixeiramarina13@gmail.com>
2023-04-25 15:46:03 -0300

    atualizando

--- Api/rotas.php
+++ Api/rotas.php
@@ -1,11 +1,14 @@
 <?php
 
+use Api\Controller\CorrentistaController;
 
 $url = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
 switch($url)
 {
     case '/correntista/save':
+        CorrentistaController::salvar();
+    break;
 
     case '/correntista/entrar':
 
